8ea3ee7353e590fa9c77c2748c2650a3e1198f7d
Hazel-BD <amilyanalia76@gmail.com>
2025-03-30 17:33:41 -0300

    Add files via upload

---
+++ CineCritica/src/main/java/com/api/CineCritica/CineCriticaApplication.java
@@ -0,0 +1,13 @@
+package com.api.CineCritica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineCriticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineCriticaApplication.class, args);
+	}
+
+}
